230f5122e682a751723dc84c0192834b01a830f5
AustinYQM <a.r.mcgowan@gmail.com>
2021-05-11 19:43:49 -0500

    start

---
+++ src/main/java/com/yqmonline/gaterulerapi/GaterulerapiApplication.java
@@ -0,0 +1,13 @@
+package com.yqmonline.gaterulerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaterulerapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaterulerapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yqmonline/gaterulerapi/GaterulerapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yqmonline.gaterulerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaterulerapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
